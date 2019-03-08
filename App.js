6deca42651f0b288c69a9d0742d33a461aec88c0
Ben <btb331@gmail.com>
2019-03-08 17:22:02 +0000

    start block animation

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Animated, TouchableWithoutFeedback, Text, View, StyleSheet, Easing} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Block />
     );
   }
+}
+
+class Block extends Component {
+	constructor(props) {
+		super(props)
+		startAnimation()
+	}
+
+	 startAnimation(){
+	 	this.moveAnimation = new Animated.ValueXY({x:400, y:300})
+	    Animated.timing(this.moveAnimation, {
+	      toValue: {x: -10, y: 300},
+	      duration: 5000,
+	      easing: Easing.linear
+	    }).start(this.startAnimation())
+	}
+
+
+	render() {
+	    return (
+	      <View style={styles.container}>
+	        <Animated.View style={[styles.rectangle, this.moveAnimation.getLayout()]}>    
+	        </Animated.View>
+	      </View>
+	    );
+	}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
+rectangle: {
+    width: 10,
+    height: 10,
+    backgroundColor: 'red'
+},
+  button: {
+    paddingTop: 24,
+    paddingBottom: 24,
+  },
+  buttonText: {
+    fontSize: 24,
+    color: '#333',
+  }
 });
