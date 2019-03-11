270e7fb42331318a319b7cbb15c07b418a9ffe18
Ben <btb331@gmail.com>
2019-03-11 09:00:12 +0000

    Animating block

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Animated, TouchableWithoutFeedback, Text, View, StyleSheet, Easing} from 'react-native';
+import { Alert, Animated, TouchableWithoutFeedback, Text, View, StyleSheet, Easing} from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -12,45 +12,66 @@ export default class App extends React.Component {
 class Block extends Component {
 	constructor(props) {
 		super(props)
-		startAnimation()
+		this.animatedValue = new Animated.Value(0)
+  this.state = {col:"red"}
 	}
+	componentDidMount () {
+  this.animate(0.5)
+}
 
-	 startAnimation(){
-	 	this.moveAnimation = new Animated.ValueXY({x:400, y:300})
-	    Animated.timing(this.moveAnimation, {
-	      toValue: {x: -10, y: 300},
-	      duration: 5000,
-	      easing: Easing.linear
-	    }).start(this.startAnimation())
-	}
+animate (pos) {
+  this.setState({col: this.chooseCol()})
+  this.animatedValue.setValue(pos)
+  Animated.timing(
+    this.animatedValue,
+    {
+      toValue: 1,
+      duration: 2750,
+      easing: Easing.linear
+    }
+  ).start(() => this.animate(0))
+}
 
+chooseCol(){
+	var col = "red"
+	var num = Math.random()
+	// Alert.alert(num)
+  if(num<0.75){
+  	col = "green"
+  }
+  if(num<0.5){
+  	col="black"
+  }
+  if(num<0.25){
+  	col = "yellow"
+  }
 
-	render() {
-	    return (
-	      <View style={styles.container}>
-	        <Animated.View style={[styles.rectangle, this.moveAnimation.getLayout()]}>    
-	        </Animated.View>
-	      </View>
-	    );
-	}
+  return col
+} 
+
+render () { 
+  const marginLeft = this.animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [380, -19]
+  })
+
+	return (
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          marginLeft,
+          height: 5,
+          width: 20,
+          top: 300,
+          backgroundColor: this.state.col}}>
+      </Animated.View>
+    </View>
+)}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ecf0f1',
-  },
-rectangle: {
-    width: 10,
-    height: 10,
-    backgroundColor: 'red'
-},
-  button: {
-    paddingTop: 24,
-    paddingBottom: 24,
-  },
-  buttonText: {
-    fontSize: 24,
-    color: '#333',
   }
 });
